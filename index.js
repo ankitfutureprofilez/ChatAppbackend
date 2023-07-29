732732ca9f52de37e6936ba35df8a5fc021c049c
ankit jain <ankit.jain@futureprofilez.com>
2023-07-29 16:01:04 +0530

    dddssss

--- index.js
+++ index.js
@@ -9,18 +9,7 @@ const app = express();
 
 const cors = require("cors")
 
-var allowedOrigins = ['http://localhost:3000', 'http://localhost'];
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+app.use(cors())
 
 const server = http.createServer(app);
 

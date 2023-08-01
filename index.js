0fc6b04d6dffff4bed25bb08fb6c734b0f4e46c7
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2023-08-01 11:44:11 +0530

    vercel last

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ const Chat = require('./models/Messages'); // Assuming the correct path to your 
 
 const io = new Server(server, {
     cors: {
-        origin: "*", // Change this to the frontend's URL
+        origin: "https://chat-app-silk-pi.vercel.app/", // Change this to the frontend's URL
         methods: ['GET', 'POST'],
     },
 });

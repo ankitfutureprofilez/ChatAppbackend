b8184861e33c12fab534f4cb3e13a28d9671a840
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2023-08-03 14:37:54 +0530

    vercel last

--- controller/chatController.js
+++ controller/chatController.js
@@ -16,44 +16,41 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
-
 exports.findAnswer = async (req, res) => {
   try {
     const userQuestion = req.body.question;
-    const fields = ['React.js', 'Node.js', 'PHP', 'react js']; // Specify the relevant fields
-    const companyDetails = "My company name future profilez .my company owner name Vishal ,  Company Stablished in 2005 .  it is a web development company in jaipur india.my Company Working Time is 9:30 AM  to 6:30 PM .My Company work it PHP, laravel, shopify, mangto and Mern Stack.my Company Google Review 4.9 in all time . my company provide multiple Services  Mobile, E - business,   PHP, Laravel Development, CakePHP Development, Zend Development, CodeIgniter Development, Yii Development, Custom PHP Development, PHP MySQL Development  my company Location is  Office No.D - 105B, G - 4, Golden OAK - 1, Devi Marg, Bani Park, Jaipur, Rajasthan 302016. "; // Pass your company details in the request body
-    if (!userQuestion || !companyDetails) {
+    if (!userQuestion) {
       return res.status(400).json({
-        msg: 'Bad Request: Missing question or companyDetails field in the request body.',
+        msg: 'Bad Request: Missing question field in the request body.',
         status: 400,
       });
     }
 
-    // Combine all the fields, company details, and the user's question in the prompt
-    const prompt = `${companyDetails} ${fields.map((field) => `In the field of ${field},`).join(' ')} ${userQuestion}`;
+    // Check if the user's question is related to web development
+    const isWebDevelopmentQuestion = isWebDevelopmentRelatedQuestion(userQuestion);
 
-    const completion = await openai.createCompletion({
-      model: 'text-davinci-002',
-      prompt: prompt
-    });
+    let assistantAnswer;
 
-    const assistantAnswer = completion.data.choices[0].text;
-
-    // Filter the answer to ensure it relates to one of the specified fields
-    const relevantField = fields.find((field) => assistantAnswer.toLowerCase().includes(field.toLowerCase()));
-    if (!relevantField) {
-      return res.status(200).json({
-        data: "Sorry, I couldn't find a relevant answer in the specified fields.",
-        msg: 'Successfully Retrieved Answer',
+    if (isWebDevelopmentQuestion) {
+      // If the question is related to web development, use AI-generated answer
+      const completion = await openai.complete({
+        engine: 'text-davinci-001',
+        prompt: userQuestion,
+        maxTokens: 150, // Adjust the maxTokens based on how long you want the answer to be
       });
+      assistantAnswer = completion.data.choices[0].text;
+    } else {
+      // If the question is not related to web development, reply with a default message
+      assistantAnswer = "I am not fielded this type of question.";
     }
 
+    // Save the user question and the assistant's answer to the MongoDB collection
     const savedEntry = await QuestionAnswer.create({
       question: userQuestion,
       answer: assistantAnswer,
-      field: relevantField, // Save the specific field along with the question and answer
     });
 
+    // Send the answer as a response to the client
     res.json({
       status: 200,
       data: assistantAnswer,
@@ -69,6 +66,75 @@ exports.findAnswer = async (req, res) => {
     });
   }
 };
+
+
+
+// Helper function to check if a question is related to web development
+function isWebDevelopmentRelatedQuestion(question) {
+  // Implement your logic to determine if the question is related to web development
+  // For example, you can check for keywords related to web development in the question
+  const webDevKeywords = ['web development', 'frontend', 'backend', 'HTML', 'CSS', 'JavaScript', 'framework'];
+  return webDevKeywords.some((keyword) => question.toLowerCase().includes(keyword.toLowerCase()));
+}
+
+
+
+
+
+
+
+// exports.findAnswer = async (req, res) => {
+//   try {
+//     const userQuestion = req.body.question;
+//     const fields = ['React.js', 'Node.js', 'PHP', 'react js']; // Specify the relevant fields
+//     const companyDetails = "My company name future profilez .my company owner name Vishal ,  Company Stablished in 2005 .  it is a web development company in jaipur india.my Company Working Time is 9:30 AM  to 6:30 PM .My Company work it PHP, laravel, shopify, mangto and Mern Stack.my Company Google Review 4.9 in all time . my company provide multiple Services  Mobile, E - business,   PHP, Laravel Development, CakePHP Development, Zend Development, CodeIgniter Development, Yii Development, Custom PHP Development, PHP MySQL Development  my company Location is  Office No.D - 105B, G - 4, Golden OAK - 1, Devi Marg, Bani Park, Jaipur, Rajasthan 302016. "; // Pass your company details in the request body
+//     if (!userQuestion || !companyDetails) {                      
+//       return res.status(400).json({                
+//         msg: 'Bad Request: Missing question or companyDetails field in the request body.',
+//         status: 400,
+//       });   
+//     }  
+  
+//     // Combine all the fields, company details, and the user's question in the prompt
+//     const prompt = `${companyDetails} ${fields.map((field) => `In the field of ${field},`).join(' ')} ${userQuestion}`;
+
+//     const completion = await openai.createCompletion({
+//       model: 'text-davinci-002',
+//       prompt: prompt
+//     });
+
+//     const assistantAnswer = completion.data.choices[0].text;
+
+//     // Filter the answer to ensure it relates to one of the specified fields
+//     const relevantField = fields.find((field) => assistantAnswer.toLowerCase().includes(field.toLowerCase()));
+//     if (!relevantField) {
+//       return res.status(200).json({
+//         data: "Sorry, I couldn't find a relevant answer in the specified fields.",
+//         msg: 'Successfully Retrieved Answer',
+//       });
+//     }
+
+//     const savedEntry = await QuestionAnswer.create({
+//       question: userQuestion,
+//       answer: assistantAnswer,
+//       field: relevantField, // Save the specific field along with the question and answer
+//     });
+
+//     res.json({
+//       status: 200,
+//       data: assistantAnswer,
+//       msg: 'Successfully Retrieved Answer',
+//       savedEntry: savedEntry, // Optional: Send the saved database entry back in the response
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.json({
+//       err: err,
+//       msg: 'Error Detected',
+//       status: 400,
+//     });
+//   }
+// };
 
 
 exports.conversion = (async (req, res) => {
